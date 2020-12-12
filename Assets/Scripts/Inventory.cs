7f3ad5c7e1253860c7ab82fc40743668c79e3d65
z4v0 <alikdelta@wp.pl>
2020-12-12 13:07:26 +0100

    ff

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -13,9 +13,10 @@ public class Inventory : MonoBehaviour {
     }
 
     public GameObject child;
-    public enum Zasob { wood, stone, cane };
+    public enum Zasob { wood, stone, cane, borowki, mieso };
 
     public Vector3Int zasobyAmount;
+    public Vector2Int jedzeneAmount;
 
     public Slot[] slots;
 
@@ -23,13 +24,28 @@ public class Inventory : MonoBehaviour {
         slots[0].Actualize(zasobyAmount.x);
         slots[1].Actualize(zasobyAmount.y);
         slots[2].Actualize(zasobyAmount.z);
+
+        slots[3].Actualize(jedzeneAmount.x);
+        slots[7].Actualize(jedzeneAmount.y);
+
+
     }
     public void AddZasob(string newZasob, int amount) {
         if (newZasob == Zasob.wood.ToString()) zasobyAmount.x += amount;
         else if (newZasob == Zasob.stone.ToString()) zasobyAmount.y += amount;
         else if (newZasob == Zasob.cane.ToString()) zasobyAmount.z += amount;
+        else if (newZasob == Zasob.borowki.ToString()) jedzeneAmount.x += amount;
+        else if (newZasob == Zasob.mieso.ToString()) jedzeneAmount.y += amount;
 
         ActualizeSlots();
+    }
+    public int GetZasob(string newZasob) {
+        if (newZasob == Zasob.wood.ToString()) return zasobyAmount.x;
+        else if (newZasob == Zasob.stone.ToString()) return zasobyAmount.y;
+        else if (newZasob == Zasob.cane.ToString()) return zasobyAmount.z;
+        else if (newZasob == Zasob.borowki.ToString()) return jedzeneAmount.x;
+        else if (newZasob == Zasob.mieso.ToString()) return jedzeneAmount.y;
+        else return 0;
     }
 
     public void Void1() {
