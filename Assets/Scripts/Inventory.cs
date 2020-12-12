b593914fd97e0a50cf29b52d0cb52f9204cd03ee
z4v0 <alikdelta@wp.pl>
2020-12-12 12:46:53 +0100

    fg

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,11 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour {
 
+    public static Inventory inventory;
+    private void Awake() {
+        inventory = this;
+    }
+
     public GameObject child;
+    public enum Zasob { wood, stone, cane };
+
+    public Vector3Int zasobyAmount;
+
+    public Slot[] slots;
+
+    public void ActualizeSlots() {
+        slots[0].Actualize(zasobyAmount.x);
+        slots[1].Actualize(zasobyAmount.y);
+        slots[2].Actualize(zasobyAmount.z);
+    }
+    public void AddZasob(string newZasob, int amount) {
+        if (newZasob == Zasob.wood.ToString()) zasobyAmount.x += amount;
+        else if (newZasob == Zasob.stone.ToString()) zasobyAmount.y += amount;
+        else if (newZasob == Zasob.cane.ToString()) zasobyAmount.z += amount;
+    }
+
     public void Void1() {
         child.SetActive(true);
     }
---
+++ Assets/Scripts/ZasobPickUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZasobPickUp : EventHolder {
+
+    public Inventory.Zasob zasob;
+    public int amount;
+
+    public override void OurStart() {
+
+    }
+    public override void TriggerEvent() {
+        Inventory.inventory.AddZasob(zasob.ToString(), amount);
+        Destroy(gameObject);
+    }
+}
