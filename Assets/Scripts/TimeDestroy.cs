853c76f4608932bdbeb3992cb8ca4c387b2e6fc3
z4v0 <alikdelta@wp.pl>
2020-12-12 14:53:41 +0100

    ftgyhjk

---
+++ Assets/Scripts/TimeDestroy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDestroy : MonoBehaviour {
+    public float timed;
+
+    void Update() {
+        timed -= Time.deltaTime;
+    }
+}
