1b32a6450f1cc71d06b2bcb39a9855176fbda870
z4v0 <alikdelta@wp.pl>
2020-12-12 12:32:03 +0100

    gj

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour {
-    
+
+    public GameObject child;
+    public void Void1() {
+        child.SetActive(true);
+    }
+    public void Void2() {
+        child.SetActive(false);
+    }
 }
